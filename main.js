604524cacc3ff653eaf5cd7e5e0142671972488e
Lesa Ducksworth <lducks67@gmail.com>
2020-07-27 23:38:04 -0500

    updated sound and image citations, adjusted setTimeout

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-// all audio effects from https://www.freesoundeffects.com/
-// Sound from Zapsplat.com
-// images from 
+//Audio and Image Credits
+// Sounds from: Zapsplat.com
+// images from:
+// grim reaper --> https://www.deviantart.com/nyourochi/art/Grim-Reaper-request-722635346
+// dragon --> https://www.vectorstock.com/royalty-free-vector/dragon-fire-holding-basketball-isolated-retro-vector-8293184
+// unicorn --> https://pixels.com/featured/1-unicorn-basketball-player-mister-tee.html
+// leprechaun --> https://www.redbubble.com/i/poster/Leprechaun-Basketball-T-Shirt-St-Patricks-Day-Boys-Kids-Men-by-LiqueGifts/30618900.LVTDI
